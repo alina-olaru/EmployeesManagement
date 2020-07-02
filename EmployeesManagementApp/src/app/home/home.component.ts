c59151b98dce7578c09e03739a88332bb69d2cbf
Alina Olaru <olarualina01@gmail.com>
2020-07-02 15:53:23 +0300

    getting data from json file using BehaviorSubject.

--- EmployeesManagementApp/src/app/home/home.component.ts
+++ EmployeesManagementApp/src/app/home/home.component.ts
@@ -1,15 +1,30 @@
+import { Observable } from 'rxjs';
+import { GetDataService } from './../services/get-data.service';
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Employee } from 'src/models/employee';
+//import * as data from '../../assets/employees.json';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+employees : Observable<Employee[]>;
 
-  constructor() { }
+  constructor(private getDataService : GetDataService,
+    ) { }
 
   ngOnInit(): void {
+  //  console.log(data);
+  this.getData();
   }
+
+  getData(){
+  this.employees= this.getDataService.subscribeToEmployees();
+  console.log(this.employees);
+  console.log(this.employees.subscribe()); //.value.forEach(el => console.log(el));
+  }
+
 
 }
---
+++ EmployeesManagementApp/src/app/services/get-data.service.ts
@@ -0,0 +1,31 @@
+import { Employee } from './../../models/employee';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GetDataService {
+
+  employees: BehaviorSubject<Employee[]>;
+
+constructor(private http: HttpClient) {
+  this.fetchData();
+ }
+
+
+fetchData() {
+  this.employees = ( new BehaviorSubject(new Array<Employee>()) as BehaviorSubject<Employee[]>);
+  this.http.get<Employee[]>('../../assets/employees.json').subscribe((data: Employee[]) => {
+    console.log("in fetch data" + data);
+    this.employees.next(data);
+  }),
+  error => console.log(error);
+}
+
+subscribeToEmployees(): Observable<Employee[]>{
+  return this.employees.asObservable();
+}
+
+}
