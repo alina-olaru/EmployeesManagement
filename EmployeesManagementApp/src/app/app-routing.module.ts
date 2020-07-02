bed8293d4844a20160e1d1b34b5a1137e93f5193
Alina Olaru <olarualina01@gmail.com>
2020-07-02 13:47:21 +0300

    Home page created via Angular CLI & routing added ( this home page is default route for our web app)

--- EmployeesManagementApp/src/app/app-routing.module.ts
+++ EmployeesManagementApp/src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: HomeComponent
+  },
+  {
+    path:'home',
+    component: HomeComponent
+  },
+  {
+    path:'**',
+    redirectTo:'/home',
+    pathMatch:'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
